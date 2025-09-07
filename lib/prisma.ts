47956c9ff7a28ffc7507b95f3b2058da2dd2fcbb
Shloimie <hshloimie@gmail.com>
2025-09-07 12:42:24 -0400

    so many db issues

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,13 @@
-// /lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ log: ["warn","error"] });
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+const g = globalThis as unknown as { prisma?: PrismaClient };
+
+export const prisma =
+    g.prisma ??
+    new PrismaClient({
+        log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+    });
+
+if (process.env.NODE_ENV !== "production") g.prisma = prisma;
+
 export default prisma;
